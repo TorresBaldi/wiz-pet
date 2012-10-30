7810ef5a13c0185849f815d3aa045726aba9994e
Tehuel Torres Baldi <torresbaldi.tehuel@gmail.com>
2012-10-30 18:02:38 -0300

    updated fpg construction

--- main.bat
+++ main.bat
@@ -2,8 +2,13 @@
 cd /d %~dp0
 cls
 
+:: CREO FPGS
+..\..\bin\bgdi.exe build-fpgs.dcb
+
+
 :: COMPILO PRG
 ..\..\bin\bgdc.exe -g main.prg
+
 
 :: EN CASO CORRECTO EJECUTO
 IF NOT ERRORLEVEL 2 ..\..\bin\bgdi.exe main.dcb
