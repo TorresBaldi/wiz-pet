cd6e362cfefda5f8acd4c900d0712e2bbfe16c0e
Tehuel Torres Baldi <torresbaldi.tehuel@gmail.com>
2012-11-02 02:26:53 -0300

    added exe release. updated release script

--- releases/build-releases.bat
+++ releases/build-releases.bat
@@ -4,12 +4,14 @@ Title BUILDBOT
 :::::::::::: CONFIGURACIONES
 set gamename=wizpet
 
+set bennupath="E:\Bennu\bin"
+
 set windows=1
-set exe=0
 set linux=0
 set source=1
 set wiz=1
 set canoo=1
+set exe=1
 
 
 
@@ -23,9 +25,11 @@ mkdir %ver%
 set path=%path%;"C:\Archivos de programa\WinRAR"
 
 
+
 :::::::::::: CONSTRUYO LA RELEASE
 cd ..
-releases\wizpet-windows\bin\bgdc.exe -a main.prg
+"%bennupath%\bgdc.exe" -a main.prg
+"%bennupath%\bgdc.exe" -a main.prg -s bgdi.exe -o %gamename%.exe
 cd releases\
 
 
@@ -36,7 +40,7 @@ SET short_path=0
 
 IF %windows% == 0 GOTO skip_windows
 
-	IF %exe% == 1 SET exe=2
+	ECHO Windows
 
 	SET windows=0
 	SET short_path=1
@@ -44,9 +48,9 @@ IF %windows% == 0 GOTO skip_windows
 	GOTO copy_files
 :skip_windows
 
-SET exe=0
-
 IF %linux% == 0 GOTO skip_linux
+
+	ECHO Linux
 
 	SET linux=0
 	SET short_path=1
@@ -55,18 +59,37 @@ IF %linux% == 0 GOTO skip_linux
 :skip_linux
 
 IF %wiz% == 0 GOTO skip_wiz
+
+	ECHO Wiz
+
 	SET wiz=0
 	SET platform=wiz
 	GOTO copy_files
 :skip_wiz
 
 IF %canoo% == 0 GOTO skip_canoo
+
+	ECHO Canoo
+
 	SET canoo=0
 	SET platform=canoo
 	GOTO copy_files
 :skip_canoo
 
+IF %exe% == 0 GOTO skip_exe
+
+	ECHO Windows Exe
+
+	SET exe=2
+	SET short_path=1
+	SET platform=exe
+	GOTO copy_files
+:skip_exe
+
 IF %source% == 0 GOTO skip_source
+
+	ECHO Source Code
+
 	SET source=0
 	SET platform=source
 	GOTO copy_source
@@ -81,12 +104,23 @@ GOTO bot_end
 SET URL=%gamename%-%platform%\%gamename%
 IF %short_path% == 1 SET URL=%gamename%-%platform%
 
+:: copio archivos
 copy ..\main.dcb %URL%\
 
-winRAR a -cl -m5 -r %ver%\%gamename%-%ver%-%platform%.zip %gamename%-%platform%\*
-IF %exe% == 2 winRAR s -zsfx.txt %ver%\%gamename%-%ver%-%platform%.zip
+:: si es version exe copio el exe
+IF %exe% == 2 del %URL%\main.dcb
+IF %exe% == 2 copy ..\%gamename%.exe %URL%\
 
+::comprimo archivo
+winRAR a -cl -m5 -r %ver%\%gamename%-%ver%-%platform%.zip %gamename%-%platform%\*
+
+::borro archivos
 del %URL%\main.dcb
+IF %exe%==2 del %URL%\%gamename%.exe
+
+:: indico que ya se construyo el exe
+IF %exe%==2 SET exe=0
+
 
 GOTO bot_begin
 
@@ -115,6 +149,10 @@ GOTO bot_begin
 
 :::::::::::: FINAL
 :bot_end
+
+:: borro los archivos creados
+del ..\main.dcb
+del ..\main.exe
 
 ::creo un archivo con todas las releases juntas
 winRAR a -cl -m5 -r -ep1 %ver%\%gamename%-all.zip %ver%\*
--- releases/wizpet-windows/wizpet.bat
+++ releases/wizpet-windows/wizpet.bat
@@ -1,5 +1,3 @@
 @echo off
 cd /d %~dp0
-
-bin\bgdc main.prg
 bin\bgdi main.dcb
